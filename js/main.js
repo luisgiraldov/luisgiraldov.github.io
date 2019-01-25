5db348a926f702e3c0ae196f3ea7c44c84153e7f
Luis Giraldo <lugirva@gmail.com>
2019-01-25 11:48:13 -0500

    fixing the social media links

--- js/main.js
+++ js/main.js
@@ -1,199 +1,244 @@
   'use strict';
 
+  //when everything has loaded
+  function ready() {
+    /*
+     * Check the viewport width
+     */
+    function getWidth() {
+      let winWidth = window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+      return winWidth;
+    }; //End getWidth();
 
+    /*
+     * Check the viewport height
+     */
+    const getHeight = () => {
+      let winHeight = window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight;
+      return winHeight;
+    }; //End getHeight();
 
-
-//Prueba to get the width of the viewport
-//when everything has loaded
-function ready() {
-  let counter = 0;
-  /*
-   * Check the viewport width
-   */
-  function getWidth() {
-    let winWidth = window.innerWidth
-                   || document.documentElement.clientWidth
-                   || document.body.clientWidth;
-    return winWidth;
-  }; //End getWidth();
-
-  /*
-   * Check the viewport height
-   */
-  const getHeight = () => {
-    let winHeight = window.innerHeight
-                   || document.documentElement.clientHeight
-                   || document.body.clientHeight;
-    return winHeight;
-  }; //End getHeight();
-
-  /*
-    tablets breakpoint : 768px and up
-    desktop breakpoint : 992px and up
-  */
-
-  /*
-   * Check if the viewport width is between tablets and desktops
-   */
-   function checkDeviceSize() {
-     let winWidth = getWidth();
-
-     if (winWidth >= 768 && winWidth < 992) {
-       //It is in a tablet screen size
-       putImageAsChild();
-     } else {
-       putImageAsSibling();
-     }
-   };
-
-   let resizeTimer;
-   $(window).resize(function() {
-     clearTimeout(resizeTimer);
-     resizeTimer = setTimeout(checkDeviceSize, 10);
-   });
-
-   /*
-    * Take the images with the class projectSectionJs being a sibling node of projectSectionText and put it as a child node
-    * of the same(projectSectionText)(all of that when is a tablet, this will help to float the text around the image)
+    /*
+      tablets breakpoint : 768px and up
+      desktop breakpoint : 992px and up
     */
-   function putImageAsChild() {
 
-    let projectSectionJs = document.querySelectorAll(".project-section-js");
-    let projectSectionText;
-    let projectSectionContainer;
-    let containsProjectSectionTextClass;
-    for (let i = 0; i < projectSectionJs.length; i++) {
+    /*
+     * Check if the viewport width is between tablets and desktops
+     */
+    function checkDeviceSize() {
+      let winWidth = getWidth();
+
+      if (winWidth >= 768 && winWidth < 992) {
+        //It is in a tablet screen size
+        putImageAsChild();
+      } else {
+        putImageAsSibling();
+      }
+    };
+
+    //when the user resizes the screen, it calls the checkDeviceSize function
+    let resizeTimer;
+    $(window).resize(function() {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(checkDeviceSize, 10);
+    });
+
+    /*
+     * Take the images with the class projectSectionJs being a sibling node of projectSectionText and put it as a child node
+     * of the same(projectSectionText)(all of that when is a tablet, this will help to float the text around the image)
+     */
+    function putImageAsChild() {
+
+      let projectSectionJs = document.querySelectorAll(".project-section-js");
+      let projectSectionText;
+      let projectSectionContainer;
+      let containsProjectSectionTextClass;
+      for (let i = 0; i < projectSectionJs.length; i++) {
         projectSectionText = projectSectionJs[i].previousElementSibling;
         containsProjectSectionTextClass = projectSectionText.classList.contains("project-section-text");
         // console.log(projectSectionText);
-        if(containsProjectSectionTextClass){
+        if (containsProjectSectionTextClass) {
           projectSectionText.classList.add("minWidth100");
           projectSectionContainer = projectSectionText.parentNode;
           projectSectionContainer.removeChild(projectSectionJs[i]);
-          projectSectionText.insertBefore(projectSectionJs[i],projectSectionText.children[1]);
-        } else {
-          console.log("something went wrong in imageChild!!");
-        }
+          projectSectionText.insertBefore(projectSectionJs[i], projectSectionText.children[1]);
+        }// End If statement
+      }//End For loop
+    }; //End imageChild
 
-    }
-
-   }; //End imageChild
-
-   function putImageAsSibling() {
-     let projectSectionJs = document.querySelectorAll(".project-section-js");
-     let projectSectionText;
-     let projectSectionContainer;
-     let containsProjectSectionTextClass;
-     for (let i = 0; i < projectSectionJs.length; i++) {
-         projectSectionText = projectSectionJs[i].parentNode;
-         containsProjectSectionTextClass = projectSectionText.classList.contains("project-section-text");
+    function putImageAsSibling() {
+      let projectSectionJs = document.querySelectorAll(".project-section-js");
+      let projectSectionText;
+      let projectSectionContainer;
+      let containsProjectSectionTextClass;
+      for (let i = 0; i < projectSectionJs.length; i++) {
+        projectSectionText = projectSectionJs[i].parentNode;
+        containsProjectSectionTextClass = projectSectionText.classList.contains("project-section-text");
         //  console.log(projectSectionText);
-        if(containsProjectSectionTextClass){
+        if (containsProjectSectionTextClass) {
           projectSectionText.classList.remove("minWidth100");
           projectSectionContainer = projectSectionText.parentNode;
           projectSectionText.removeChild(projectSectionJs[i]);
           projectSectionContainer.appendChild(projectSectionJs[i]);
+        } //End If statement
+      }//End for loop
+    };// End Image sibling
+
+    checkDeviceSize();
+
+
+    /*
+     * Smooth Scrolling Effect with Accesibility Compliance
+     */
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }; //End of else
+            }); //End of animate method
+          } //End of If statement target.length
+        } //End of If statement (location Pathname)
+      }); //End Click method
+
+
+    /* ==========================================================================
+        Sticky Navigation Bar
+       ========================================================================== */
+    let navVariables = (function() {
+      // Get the navbar
+      let navbar = document.getElementById("navbarJs");
+      // Get the offset position of the navbar
+      let sticky = navbar.offsetTop;
+      // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+      function checkOffSet() {
+        let winHeight = getHeight();
+        if (document.body.scrollTop > winHeight || document.documentElement.scrollTop > winHeight) {
+          navbar.classList.add("sticky")
         } else {
-          console.log("Something went wrong in imageSibling!!!");
+          navbar.classList.remove("sticky");
         }
+      }
 
-     }
+      return {
+        checkOffSet: checkOffSet
+      }
 
-   };
+    }());
+    // When the user scrolls the page, execute checkOffSet
+    window.onscroll = function() {
+      navVariables.checkOffSet();
+    };
 
-   checkDeviceSize();
+    /* ==========================================================================
+        Tooltip and Popover
+       ========================================================================== */
+    /*
+     * show the popover when the user clicks, and the tooltip on hover
+     */
+    //get the anchor tag (envelope)
+    let mailBtn = $('.popover-dismiss-js');
+
+    //show the popover getting the content and title from data-attributes
+    mailBtn.popover({
+      content: mailBtn.attr('popover-content'),
+      title: mailBtn.attr('popover-title')
+    });
+
+    //when the envelope (button) loses focus, it hides the popover
+    mailBtn.on('blur', function() {
+      mailBtn.popover('hide');
+    });
+
+    //shows the tooltip, getting data from data-attributes
+    mailBtn.tooltip({
+      placement: 'bottom',
+      title: mailBtn.attr('tooltip-title'),
+      trigger: 'hover'
+    });
+
+    //when the popover shows up, the tooltip will hide.
+    mailBtn.on('show.bs.popover', function() {
+      mailBtn.tooltip('hide')
+    });
+    // End of showing the tooltip and popover
 
 
-/*
- * Smooth Scrolling Effect with Accesibility Compliance
- */
-
- // Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          }; //End of else
-        }); //End of animate method
-      } //End of If statement target.length
-    } //End of If statement (location Pathname)
-  }); //End Click method
-
-  /* ==========================================================================
-      back to top button
-     ========================================================================== */
-
-  // When the user scrolls down 500px from the top of the document, show the button
-  // window.onscroll = function() {scrollFunction()};
-  // let backToTop = document.getElementById("back-to-top-js");
-  // function scrollFunction() {
-  //    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-  //      backToTop.style.display = "block";
-  //      setTimeout(function() {
-  //        backToTop.style.display = "none";
-  //      }, 5000);
-  //    } else {
-  //      backToTop.style.display = "none";
-  //    }
-  //  }
-
+  }; //End ready()
+  window.onload = ready();
 
   /* ==========================================================================
-      Sticky Navigation Bar
+      Copy data-attributes to the clipboard
      ========================================================================== */
-     let navVariables = (function() {
-       // Get the navbar
-       let navbar = document.getElementById("navbarJs");
-       // Get the offset position of the navbar
-       let sticky = navbar.offsetTop;
-       console.log(sticky + " inside navVariables")
-       // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-       function checkOffSet() {
-         let winHeight = getHeight();
-         if (document.body.scrollTop > winHeight || document.documentElement.scrollTop > winHeight) {
-           navbar.classList.add("sticky")
-         } else {
-           navbar.classList.remove("sticky");
-         }
-       }
 
-       return {
-         checkOffSet : checkOffSet
-       }
+  //copy text to the clipboard
+  (function() {
+    function createNode(text) {
+      var node = document.createElement('pre');
+      node.style.width = '1px';
+      node.style.height = '1px';
+      node.style.position = 'fixed';
+      node.style.top = '5px';
+      node.textContent = text;
+      return node;
+    }
 
-     }());
-     // When the user scrolls the page, execute checkOffSet
-     window.onscroll = function() {
-        navVariables.checkOffSet();
-     };
+    function copyNode(node) {
+      var selection = getSelection();
+      selection.removeAllRanges();
 
+      var range = document.createRange();
+      range.selectNodeContents(node);
+      selection.addRange(range);
 
+      document.execCommand('copy');
+      selection.removeAllRanges();
+    }
 
-}; //End ready()
-window.onload = ready();
+    function copyText(text) {
+      var node = createNode(text);
+      document.body.appendChild(node);
+      copyNode(node);
+      document.body.removeChild(node);
+    }
+
+    $('.popover-dismiss-js').on('click', function() {
+      var text;
+      if (text = this.getAttribute('data-email')) {
+        copyText(text);
+      }
+
+    });
+  }).call(this);
+  //end of copying text to the clipboard
